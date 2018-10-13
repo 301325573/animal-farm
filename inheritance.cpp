096108ff4da3cd6bf1166976a5a0e3f2bb01ce14
Julian Kim <jkim671@my.bcit.ca>
2018-10-12 17:35:05 -0700

    Implemented canine and tested main

--- inheritance.cpp
+++ inheritance.cpp
@@ -32,7 +32,9 @@ public:
         location[1] = a.location[1];
     }
 
-    virtual ~Animal(){};
+    virtual ~Animal(){
+        delete[] location;
+    }
 
     friend ostream &operator << (ofstream &out, const Animal &c);
 
@@ -55,44 +57,86 @@ protected:
 public:
     Bird() {
         age = 0;
-        location[0] = 0;
-        location[1] = 1;
+        this->location[0] = 0;
+        this->location[1] = 0;
     };
 
     Bird(int age, double x, double y, double z) {
-        location[0] = x;
-        location[1] = y;
+        this->location[0] = x;
+        this->location[1] = y;
         height = z;
     }
+
     Bird(const Bird& a) {
-        age = a.age;
-        ID = IDCount++;
-        location[0] = a.location[0];
-        location[1] = a.location[1];
+        this->age = a.age;
+        this->ID = IDCount++;
+        this->location[0] = a.location[0];
+        this->location[1] = a.location[1];
         height = a.height;
-        alive = a.alive;
+        this->alive = a.alive;
     };
 
-    ~Bird() {
-        delete location[];
+    ~Bird() override {
+        delete[] location;
     }
 
 
     void move(double x, double y, double z) {
-        location[0] = x;
-        location[1] = y;
+        this->location[0] = x;
+        this->location[1] = y;
         height = z;
     }
 
     friend ostream &operator << (ofstream &out, const Bird &c);
 
-    void sleep() {
+    void sleep() override {
         cout << "bird I'm sleeping!!" << endl;
     }
-    void eat() {
+    void eat() override {
         cout << "bird tasty!!!" << endl;
     }
 
+
+};
+
+class Canine:public Animal {
+public:
+    Canine() {
+        this->age = 0;
+        this->location[0] = 0;
+        this->location[1] = 0;
+    };
+
+    Canine(int age, double x, double y) {
+        this->age = age;
+        this->location[0] = x;
+    };
+
+    Canine(const Canine& c) {
+        this->location[0] = c.location[0];
+        this->location[1] = c.location[1];
+        this->age = c.age;
+        this->ID = IDCount++;
+        this->alive = c.alive;
+    };
+
+    void move(double x, double y) {
+        this->location[0] = x;
+        this->location[1] = y;
+    };
+
+    void sleep() override {
+        cout << "dog I'm sleeping!!" << endl;
+    }
+    void eat() override {
+        cout << "dog tasty!!!" << endl;
+    }
+
+    void hunt() {
+        cout << "hunting!" << endl;
+    }
+
+    friend ostream &operator << (ofstream &out, const Canine &c);
 
 };
 
@@ -100,7 +144,23 @@ long Animal::IDCount = 0;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    Animal* asdf = new Animal();
-    Bird* bird = new Bird();
+
+    Animal* canine;
+    Animal* animal;
+    Animal* bird;
+    animal = new Animal(5, 3.3, 2.2);
+    canine = new Canine(3, 6.6, 6.0);
+    bird = new Bird(7, 5, 5, 5);
+
+    animal->eat();
+    animal->sleep();
+
+    bird->eat();
+    bird->sleep();
+
+    canine->eat();
+    canine->sleep();
+    ((Canine*)canine)->hunt();
+
     return 0;
 }
